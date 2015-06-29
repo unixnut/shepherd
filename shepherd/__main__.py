876c794b5b232e8a3aa1777cd59dbb53904a936a
Alastair Irvine <alastair@plug.org.au>
2015-06-29 22:09:07 +0800

    Improved package bootstrap script
    shepherd/__main__.py:
      + added "#! /usr/bin/python"
      + made executable
      + improved docstring
      + added robust_import()
      + checks for 3 scenarios, i.e. ways in which the script could be run
        and handles them individually, using robust_import() where appropriate
      + exits with an error message and code 99 if package can't be found

--- shepherd/__main__.py
+++ shepherd/__main__.py
@@ -1,21 +1,80 @@
+#! /usr/bin/python
 """Universal package entry point.
 
 Determines the name of the package this file resides in, imports it and runs
-its main() function.
+its main() function.  Assumes the package dir's parent is in the module path.
 
-This is similar to the method suggested by
+Usage: python -m <package_name>
+   or: python -m <package_name>.__main__
+
+Note: the former only works under Python 2.7+
+
+The method used by this script is similar to the method suggested by
 https://www.python.org/dev/peps/pep-0299/ in that sys.argv is passed to main()
 and its return value is used as the program exit code."""
  
 import sys
-from os.path import realpath, dirname, basename
+from os.path import dirname, basename, splitext, realpath, exists, islink
 
-# Determine name of package and import it.
-# Note: relative imports can't be used in non-package, but this is a non-issue
-# because this file's dir, and thereby that of the package dir has been put
-# into sys.path automatically
-package_name = basename(dirname(realpath(__file__)))
-package = __import__(package_name) 
+
+# *** FUNCTIONS ***
+def robust_import(package_name, filename):
+    """Tries to dynamically import a package and if it fails, adds the
+    package's parent dir to the module path and try again.  
+
+    @param filename: a file in the package dir
+    @return a reference to the imported package"""
+    try:
+        # assume this file's parent dir (and thereby that of the package)
+        # is already present in sys.path
+        return __import__(package_name) 
+    except ImportError, e:
+        # Scenario 1b: script is being run in-place manually
+        #
+        # We can add the package's parent dir to sys.path and retry the import
+        sys.path.insert(0, dirname(dirname(filename)))
+        ## print sys.path[0]
+        return __import__(package_name) 
+
+
+# *** MAINLINE ***
+try:
+    # -- Check how this script is being run --
+    if basename(__file__) == "__main__.py":
+        # Scenario 1: script is being run in-place (either via "python -m" or manually)
+        #
+        # Note: relative imports can't be used in non-package, i.e this script
+        package_name = basename(dirname(__file__))
+        package = robust_import(package_name, __file__)
+    else:
+        # Otherwise, assume __main__.py has been copied or symlinked
+        # elsewhere and therefore is being run under another name
+
+        # -- Test for symlinking --
+        # It's OK to use islink() because it's irrelevant if the file
+        # itself is not a symlink, but a directory along the path is
+        if islink(__file__):
+            ## exists(dirname(realpath(__file__)) + "__init__.py"): 
+
+            # Scenario 2: script is symlinked
+            #
+            # Use the directory of the file that the script points to to
+            # determine the package name
+            package_name = basename(dirname(realpath(__file__)))
+            ## print package_name
+            package = robust_import(package_name, realpath(__file__))
+        else:
+            # Scenario 3: script is a copy of__main__.py
+            # 
+            # Use the filename as a guess for the package name and hope that
+            # the package dir's parent is in the module path already, because
+            # there is no way to determine it programatically
+            package_name, ext = splitext(basename(__file__))
+            package = __import__(package_name) 
+except ImportError, e:
+    print >> sys.stderr, \
+      "%s: Error: Can't find package '%s'" % (__file__, package_name)
+    sys.exit(99)
 
 if __name__ == '__main__':
     # Note difference from https://www.python.org/dev/peps/pep-0299/:
