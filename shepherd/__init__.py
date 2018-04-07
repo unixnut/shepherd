19d09b76a82bec2c2da4ff745dca20c4d8dd1a95
Alastair Irvine <alastair@plug.org.au>
2018-04-07 16:30:44 +0800

    shepherd v1.2.2
    shepherd/formatting.py: increased instance ID column width
    
    shepherd/__init__.py: bumped version

--- shepherd/__init__.py
+++ shepherd/__init__.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # shepherd (Python script) -- Control cloud servers using the provider's API
 #
-# Version:   1.2.1
+# Version:   1.2.2
 # Copyright: (c)2015 Alastair Irvine <alastair@plug.org.au>
 # Keywords:  aws boto virsh
 # Licence:   This file is released under the GNU General Public License
@@ -39,6 +39,7 @@ Options:
 # TO-DO:
 #   + handle hosts not in in primary region
 #   + go(): create PerRegionCohort objects before taking action
+#   + detect invalid action names
 
 
 from __future__ import absolute_import
--- shepherd/formatting.py
+++ shepherd/formatting.py
@@ -4,6 +4,6 @@ from __future__ import absolute_import
 def print_host(name, id, state, msg=None, indent = "  "):
     """msg, if present, is printed (with a two-space indent) after the normal
     line."""
-    print "%-24s %-13s %s" % (name, id, state)
+    print "%-24s %-25s %s" % (name, id, state)
     if msg:
         print indent + ("\n" + indent).join(msg.split("\n"))
